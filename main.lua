090ce14cd07c3ce5005b8e00142426735387173c
villager <76953683+villonger@users.noreply.github.com>
2021-10-13 17:36:20 +0200

    oof

--- main.lua
+++ main.lua
@@ -20,16 +20,14 @@ function love.load()
 
     sprites = {}
     sprites.playerSheet = love.graphics.newImage('sprites/cirnoSheet.png')
-    sprites.enemySheet = love.graphics.newImage('sprites/enemySheet.png')
     sprites.background = love.graphics.newImage('sprites/background.jpg')
 
     local grid = anim8.newGrid(240, 240, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
-    local enemyGrid = anim8.newGrid(100, 79, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
 
     animations = {}
     animations.idle = anim8.newAnimation(grid('1-2',2), 0.1)
     animations.run = anim8.newAnimation(grid('1-2',1), 0.1)
-    animations.enemy = anim8.newAnimation(enemyGrid('1-2',1), 0.03)
+   -- animations.enemy = anim8.newAnimation(enemyGrid('1-2',1), 0.03)
 
     wf = require 'libraries/windfield/windfield'
     world = wf.newWorld(0, 800, false)
